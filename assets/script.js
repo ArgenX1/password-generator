7f5c815aeba96cbaeba02446ff54930f65f53b62
ArgenX1 <djc.arizona@gmail.com>
2021-09-21 15:43:25 -0700

    added arrays to script

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+// Assignment Code
+const generateBtn = document.querySelector("#generate");
+const lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+const upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+const numValue = ["1","2","3","4","5","6","7","8","9","0"]
+const specChar = ["!","@","#","$","%","^","&","*","(",")"]
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
